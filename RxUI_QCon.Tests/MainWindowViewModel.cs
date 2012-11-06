d64c8211a7841cc24f4e087db4f59fa81e680406
Paul Betts <paul@paulbetts.org>
2012-11-05 18:02:06 -0800

    Add a test project

---
+++ RxUI_QCon.Tests/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace RxUI_QCon.Tests
+{
+    public class MainWindowViewModelTests
+    {
+        [Fact]
+        public void ColorShouldChangeWhenValuesChange()
+        {
+            var fixture = new MainWindowViewModel();
+
+            fixture.Red = "255";
+
+            Assert.NotNull(fixture.FinalColor);
+            Assert.Equal(255, fixture.FinalColor.Color.R);
+            Assert.Equal(0, fixture.FinalColor.Color.B);
+
+            fixture.Blue = "300";
+
+            Assert.NotNull(fixture.FinalColor);
+            Assert.Equal(255, fixture.FinalColor.Color.R);
+            Assert.Equal(0, fixture.FinalColor.Color.B);
+
+            fixture.Blue = "128";
+
+            Assert.NotNull(fixture.FinalColor);
+            Assert.Equal(255, fixture.FinalColor.Color.R);
+            Assert.Equal(128, fixture.FinalColor.Color.B);
+        }
+    }
+}
