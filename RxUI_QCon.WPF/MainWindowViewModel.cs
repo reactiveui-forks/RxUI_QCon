f244210cd93017a2b2c514a1297fa5cac649090b
Paul Betts <paul@paulbetts.org>
2013-03-15 20:51:02 -0500

    Modify MainWindowViewModel to be Cocoa-friendly

--- RxUI_QCon.WPF/MainWindowViewModel.cs
+++ RxUI_QCon.WPF/MainWindowViewModel.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using ReactiveUI;
 using ReactiveUI.Xaml;
 using Newtonsoft.Json;
+
+#if MONO
+using System.Drawing;
+using SolidColorBrush = System.Nullable<System.Drawing.Color>;
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+using RestSharp;
+#else
+using System.Net.Http;
+using System.Windows.Media.Imaging;
+#endif
 
 namespace RxUI_QCon
 {
@@ -21,19 +30,19 @@ namespace RxUI_QCon
         int _Red;
         public int Red {
             get { return _Red; }
-            set { this.RaiseAndSetIfChanged(value); }
+            set { this.RaiseAndSetIfChanged(x => x.Red, value); }
         }
 
         int _Green;
         public int Green {
             get { return _Green; }
-            set { this.RaiseAndSetIfChanged(value); }
+            set { this.RaiseAndSetIfChanged(x => x.Green, value); }
         }
 
         int _Blue;
         public int Blue {
             get { return _Blue; }
-            set { this.RaiseAndSetIfChanged(value); }
+            set { this.RaiseAndSetIfChanged(x => x.Blue, value); }
         }
 
         ObservableAsPropertyHelper<SolidColorBrush> _FinalColor;
@@ -41,10 +50,17 @@ namespace RxUI_QCon
             get { return _FinalColor.Value; }
         }
 
-        ObservableAsPropertyHelper<List<BitmapImage>> _Images;
-        public List<BitmapImage> Images {
+#if MONO
+        ObservableAsPropertyHelper<IList<NSImage>> _Images;
+        public IList<NSImage> Images {
             get { return _Images.Value; }
         }
+#else
+        ObservableAsPropertyHelper<IList<BitmapImage>> _Images;
+        public IList<BitmapImage> Images {
+            get { return _Images.Value; }
+        }
+#endif
 
         public ReactiveCommand Ok { get; protected set; }
 
@@ -63,10 +79,13 @@ namespace RxUI_QCon
 
             this.WhenAny(x => x.FinalColor, x => x.Value)
                 .Throttle(TimeSpan.FromSeconds(0.7), RxApp.DeferredScheduler)
+#if MONO
+                .Select(x => imagesForColor(x.Value))
+#else
                 .Select(x => imagesForColor(x.Color))
+#endif
                 .Switch()
-                .ObserveOn(RxApp.DeferredScheduler)
-                .Select(imageListToImages)
+                .SelectMany(imageListToImages)
                 .ToProperty(this, x => x.Images);
         }
 
@@ -75,7 +94,11 @@ namespace RxUI_QCon
             byte? r = inRange(colorsAsInts.Item1), g = inRange(colorsAsInts.Item2), b = inRange(colorsAsInts.Item3);
 
             if (r == null || g == null || b == null) return null;
+#if MONO
+            return Color.FromArgb(r.Value, g.Value, b.Value);
+#else
             return Color.FromRgb(r.Value, g.Value, b.Value);
+#endif
         }
 
         static byte? inRange(int value)
@@ -87,6 +110,39 @@ namespace RxUI_QCon
             return (byte) value;
         }
 
+#if MONO
+        IObservable<ImageList> imagesForColor(Color sourceColor)
+        {
+            var queryParams = new[] {
+                new { k = "method", v = "flickr_color_search" },
+                new { k = "limit", v = "73" },
+                new { k = "offset", v = "0" },
+                new { k = "colors[0]", v = String.Format("{0:x2}{1:x2}{2:x2}", sourceColor.R, sourceColor.G, sourceColor.B) },
+                new { k = "weights[0]", v = "1" },
+            };
+
+            var client = new RestClient("http://labs.tineye.com");
+            var rq = new RestRequest("rest//");
+            foreach(var p in queryParams) {
+                rq.AddParameter(p.k, p.v, ParameterType.GetOrPost);
+            }
+
+            return Observable.Start(() => {
+                var resp = client.Execute(rq);
+                return JsonConvert.DeserializeObject<ImageList>(resp.Content);
+            }, RxApp.TaskpoolScheduler);
+        }
+
+        IObservable<IList<NSImage>> imageListToImages(ImageList imageList)
+        {
+            return imageList.result.ToObservable()
+                .Select(x => "http://img.tineye.com/flickr-images/?filepath=labs-flickr/" + x.filepath)
+                .Select(x => Observable.Start(() => NSData.FromUrl(new NSUrl(x)))).Merge(4)
+                .ObserveOn(RxApp.DeferredScheduler)
+                .Select(x => new NSImage(x))
+                .ToList();
+        }
+#else
         IObservable<ImageList> imagesForColor(Color sourceColor)
         {
             var queryParams = new[] {
@@ -110,14 +166,15 @@ namespace RxUI_QCon
                 .Select(JsonConvert.DeserializeObject<ImageList>);
         }
 
-        List<BitmapImage> imageListToImages(ImageList imageList)
+        IObservable<IList<BitmapImage>> imageListToImages(ImageList imageList)
         {
-            return imageList.result
+            return imageList.result.ToObservable(RxApp.DeferredScheduler)
                 .Select(x => "http://img.tineye.com/flickr-images/?filepath=labs-flickr/" + x.filepath)
                 .Select(x => {
                     var ret = new BitmapImage(new Uri(x));
                     return ret;
                 }).ToList();
         }
+#endif
     }
 }
