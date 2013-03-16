bc0e0f733d127892ac470dd2ae14411c6dba3ea4
Paul Betts <paul@paulbetts.org>
2013-03-15 21:50:14 -0500

    Hack around glitches in RxUI on Mac

--- RxUI_QCon.Cocoa/AppDelegate.cs
+++ RxUI_QCon.Cocoa/AppDelegate.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using MonoMac.Foundation;
 using MonoMac.AppKit;
 using MonoMac.ObjCRuntime;
+using ReactiveUI;
 
 namespace RxUI_QCon.Cocoa
 {
@@ -12,12 +13,25 @@ namespace RxUI_QCon.Cocoa
         
         public AppDelegate()
         {
+            // NB: Wherein I work around all of the RxUI bugs.
+            RxApp.Register(typeof(DummyPropertyBindingHook), typeof(IPropertyBindingHook));
+            (new ReactiveUI.Xaml.ServiceLocationRegistration()).Register();
+            (new ReactiveUI.Routing.ServiceLocationRegistration()).Register();
+            (new ReactiveUI.Cocoa.ServiceLocationRegistration()).Register();
         }
 
         public override void FinishedLaunching(NSObject notification)
         {
             mainWindowController = new MainWindowController();
             mainWindowController.Window.MakeKeyAndOrderFront(this);
+        }
+    }
+
+    public class DummyPropertyBindingHook : IPropertyBindingHook
+    {
+        public bool ExecuteHook(object source, object target, Func<IObservedChange<object, object>[]> getCurrentViewModelProperties, Func<IObservedChange<object, object>[]> getCurrentViewProperties, BindingDirection direction)
+        {
+            return true;
         }
     }
 }
